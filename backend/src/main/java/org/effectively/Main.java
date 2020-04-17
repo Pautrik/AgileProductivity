8e5008826a5e1e1abadd8afa6a93c6497d9b7f2c
Eddy <54838825+EddyIT99@users.noreply.github.com>
2020-04-17 11:36:21 +0200

    Added alpha server to backend, will need to be changed

--- backend/src/main/java/org/effectively/Main.java
+++ backend/src/main/java/org/effectively/Main.java
@@ -1,7 +1,23 @@
 package org.effectively;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.logging.Logger;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("aaa");
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8001), 0);
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+        Logger logger = Logger.getLogger(Main.class.getName());
+
+        server.createContext("/test", new MyHttpHandler());
+        server.setExecutor(threadPoolExecutor);
+        server.start();
+        logger.info(" Server started on port 8001");
     }
 }
--- backend/src/main/java/org/effectively/dataObjects/Day.java
+++ backend/src/main/java/org/effectively/dataObjects/Day.java
@@ -4,5 +4,10 @@ import java.util.ArrayList;
 
 public class Day {
     String date;
-    ArrayList<Task> Task = new ArrayList<>();
+    ArrayList<Task> tasks;
+
+    public Day(String date, ArrayList<org.effectively.dataObjects.Task> tasks) {
+        this.date = date;
+        this.tasks = tasks;
+    }
 }
