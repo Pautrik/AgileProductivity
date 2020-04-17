349472f0a0b32768a95e01b5abb96793051f7fc7
Eddy <54838825+EddyIT99@users.noreply.github.com>
2020-04-17 11:36:21 +0200

    Added alpha server to backend, will need to be changed

--- backend/src/main/java/org/effectively/dataObjects/Task.java
+++ backend/src/main/java/org/effectively/dataObjects/Task.java
@@ -3,4 +3,9 @@ package org.effectively.dataObjects;
 public class Task {
     String text;
     Integer state;
+
+    public Task(String text, Integer state) {
+        this.text = text;
+        this.state = state;
+    }
 }
