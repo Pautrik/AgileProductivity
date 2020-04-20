934cf55adde5644223eee32e10932a7db9b7e194
Eddy <54838825+EddyIT99@users.noreply.github.com>
2020-04-20 11:29:50 +0200

    Improved database handler by adding request handlers

--- backend/src/main/java/org/effectively/ServerRequestHandler.java
+++ backend/src/main/java/org/effectively/ServerRequestHandler.java
@@ -1,34 +1,36 @@
 package org.effectively;
 
 import com.sun.net.httpserver.HttpExchange;
+import com.sun.tools.javac.util.Pair;
+
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class ServerRequestHandler implements com.sun.net.httpserver.HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        String requestParamValue = null;
+        Pair<String, String> requestParamValue = null;
         if("GET".equals(httpExchange.getRequestMethod())) {
-/*
+
             requestParamValue = handleGetRequest(httpExchange); //uncomment when starting to work on custom requests
-*/
+
         }/*else if("POST".equals(httpExchange)) { //uncomment when starting to work on adding items to database
             requestParamValue = handlePostRequest(httpExchange);
         }*/
+
         handleResponse(httpExchange,requestParamValue);
     }
-    private String handleGetRequest(HttpExchange httpExchange) {
-        return httpExchange.
-        getRequestURI()
-                .toString()
-                .split("\\?")[1]
-                .split("=")[1];
+    private Pair handleGetRequest(HttpExchange httpExchange) {
+
+        String [] paramvalue = httpExchange.getRequestURI().getQuery().split("=");
+
+        return new Pair(paramvalue[0],paramvalue[1]);
     }
-    private void handleResponse(HttpExchange httpExchange, String requestParamValue)  throws  IOException {
+    private void handleResponse(HttpExchange httpExchange, Pair requestParamValue)  throws  IOException {
         OutputStream outputStream = httpExchange.getResponseBody();
 
         //Request the wanted data from DatabaseHandler
-        String jsonResponse = DatabaseHandler.requestData();
+        String jsonResponse = DatabaseHandler.requestData(requestParamValue);
 
         httpExchange.sendResponseHeaders(200, jsonResponse.length());
         outputStream.write(jsonResponse.getBytes());
