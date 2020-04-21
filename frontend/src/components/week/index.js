2b4fa0acabfc586f2e15fb864c0ba042f46db6c8
Adam Ingemarsson <adam@Adams-MacBook-Pro.local>
2020-04-21 10:59:47 +0200

    Finished week-view, added notes

--- frontend/src/components/week/index.js
+++ frontend/src/components/week/index.js
@@ -5,15 +5,25 @@ import './index.css';
 class Week extends React.Component {
     render() {
         return (
-            <div className="week-box">
-                <div className="week-header">
-                    <h1>
-                        January 2020
-                    </h1>
+            <div className="week-view">
+                <div className="week">
+                    <div className="week-header">
+                        <h1>
+                            January 2020
+                        </h1>
+                    </div>
+                    <div className="days">
+                        <Day dayName="Monday"/>
+                        <Day dayName="Tuesday"/>
+                        <Day dayName="Wensday"/>
+                        <Day dayName="Thursday"/>
+                        <Day dayName="Friday"/>
+                        <Day dayName="Saturday"/>
+                        <Day dayName="Sunday"/>
+                    </div>
                 </div>
-                <div className="days">
-                    <Day dayName="Monday"/>
-                    <Day dayName="Tuesday"/>
+                <div className="note-container">
+                    <Day dayName="Notes" />
                 </div>
             </div>
         )
