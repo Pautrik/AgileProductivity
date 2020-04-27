a5a9d5ed2d3fc16a1a4ed565eb833cc81f674731
johanD98 <dajo9999@hotmail.se>
2020-04-27 12:53:51 +0200

    added two buttons to tasks and changed some colors

--- frontend/src/components/tasks/index.js
+++ frontend/src/components/tasks/index.js
@@ -1,9 +1,14 @@
 import "./index.css";
 import React from "react";
+import Button from "../button";
 
 class Task extends React.Component {
   render() {
-    return <div className="task">{this.props.taskText} </div>;
+    return <div className="task">
+        <button className="x-button">X</button>
+        {this.props.taskText}
+        <button className="done-button">Done</button>
+    </div>;
   }
 }
 
