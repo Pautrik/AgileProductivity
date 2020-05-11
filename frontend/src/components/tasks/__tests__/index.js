0389d4b5425164a260e7416ac259cb973061c260
pautrik <>
2020-05-11 14:31:20 +0200

    Added more tests to <Task />

--- frontend/src/components/tasks/__tests__/index.js
+++ frontend/src/components/tasks/__tests__/index.js
@@ -5,15 +5,31 @@ import Task from "../";
 
 const taskText = "TestTestTest";
 
+const renderTask = (status = 1) => render(<Task status={status} taskText={taskText}></Task>);
 
 describe("<Task/>", () => {
     it("Renders taskText", () => {
-        const { getByText } = render(<Task taskText={taskText}></Task>);
+        const { getByText } = renderTask();
         expect(getByText(taskText)).toBeInTheDocument();
     });
 
-    it("Renders done button", () => {
-        const { getByText } = render(<Task taskText={taskText}></Task>);
+    it("Renders X button", () => {
+        const { getByText } = renderTask();
+        expect(getByText("X", { exact: false })).toBeInTheDocument();
+    });
+
+    it("Renders 'Start' button", () => {
+        const { getByText } = renderTask(1);
+        expect(getByText("Start", { exact: false })).toBeInTheDocument();
+    });
+
+    it("Renders 'Done' button", () => {
+        const { getByText } = renderTask(2);
         expect(getByText("Done", { exact: false })).toBeInTheDocument();
+    });
+
+    it("Renders 'Archive' button", () => {
+        const { getByText } = renderTask(3);
+        expect(getByText("Archive", { exact: false })).toBeInTheDocument();
     });
 });
