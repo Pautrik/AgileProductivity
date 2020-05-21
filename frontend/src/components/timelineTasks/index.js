fb0c402ecd1b948d81ddf131c798074d7fc7d4d4
Adam Ingemarsson <adam@Adams-MacBook-Pro.local>
2020-05-21 13:37:56 +0200

    timelinetasks added

---
+++ frontend/src/components/timelineTasks/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+import React from "react";
+
+
+class timelineTasks extends React.Component {
+  render() {
+    <div className="timeline-task">
+        
+    </div>
+  }
+}
+
+export default timelineTasks;
