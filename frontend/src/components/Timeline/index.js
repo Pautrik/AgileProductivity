54f1acaa6fd6c0d46eb61c11461e0718602eb7c8
johanD98 <dajo9999@hotmail.se>
2020-05-22 16:36:22 +0200

    made days map from the fetch from backend

--- frontend/src/components/Timeline/index.js
+++ frontend/src/components/Timeline/index.js
@@ -3,6 +3,7 @@ import "./index.css";
 import ProjectTask from "../projectTasks"
 import {range} from "../../helpers/array";
 import { httpRequestJson } from "../../helpers/requests";
+import day from "../day";
 
 const projectEndpoint = activeState => `/projects?select=${activeState}`;
 const timelineTaskEndpoint = (projects, startDate, endDate) => `/timeline?parameters=${projects.join("&")}&${startDate}&${endDate}`;
@@ -49,6 +50,8 @@ class Timeline extends React.Component{
             projectState: 0,
             projects: [],
             tasks: [],
+            days: [],
+            isLoading: false,
         }
 
         this.scrollerRef = React.createRef();
@@ -60,12 +63,37 @@ class Timeline extends React.Component{
     }
 
     fetchProjectsAndTasksToState() {
-        const projectStates = ["any", "active", "inactive"];
-        httpRequestJson(projectEndpoint(projectStates[this.state.projectState]))
-            .then(projects => {
-                httpRequestJson(timelineTaskEndpoint(projects.map(x => x.name), "20200401", "20200427"))
-                    .then(tasks => this.setState({ projects, tasks }, () => console.log(this.state)));
-            })
+        return new Promise((resolve, reject) => {
+            this.setState({isLoading: true});
+            const projectStates = ["any", "active", "inactive"];
+            httpRequestJson(projectEndpoint(projectStates[this.state.projectState]))
+                .then(projects => {
+                    httpRequestJson(timelineTaskEndpoint(projects.map(x => x.name), this.dateToString(this.state.startDate), "20201011"))
+                    .then(tasks => {
+                        const days = this.tasksToDays(tasks, projects);
+                        this.setState({ isLoading: false, tasks, projects, days }, () => resolve());
+                    });
+                });
+        })
+    }
+
+    tasksToDays(tasks, projects) {
+        const days = [];
+        for(let i = 0; i < this.state.rangeT; i++){
+            const dayDate = new Date();
+            dayDate.setDate(this.state.startDate.getDate() + i);
+            days.push({
+                date: dayDate,
+                tasks: [],
+            });
+        }
+
+        for(let day of days) {
+            day.tasks = tasks.filter(task =>
+                task.date === this.dateToString(day.date));
+        }
+
+        return days;
     }
 
     getNextDay(x){
@@ -75,23 +103,25 @@ class Timeline extends React.Component{
     }
 
     getWeekDay(x){
-        const y = new Date();
+        const y = this.stringToDate(this.dateToString(this.state.startDate));
         y.setDate(this.state.startDate.getDate() + x);
         return weekDays[y.getDay()];
     }
 
     getMonth(x) {
-        const y = new Date();
-        y.setDate(this.state.startDate.getDate() + x);
+        const y = this.stringToDate(this.dateToString(this.state.startDate));
+        y.setDate(y.getDate() + x);
         return months[y.getMonth() - 1];
     }
 
     goBack(){
         const scrollWidth = this.scrollerRef.current.scrollWidth;
-        const newDate = new Date();
-        newDate.setDate(this.state.startDate.getDate() - 7)
+        const newDate = this.stringToDate(this.dateToString(this.state.startDate));
+        newDate.setDate(newDate.getDate() - 7)
         this.setState({startDate: newDate, rangeT: this.state.rangeT + 7}, () =>
-            this.scrollerRef.current.scrollLeft += this.scrollerRef.current.scrollWidth - scrollWidth);
+            this.fetchProjectsAndTasksToState()
+                .then(() => this.scrollerRef.current.scrollLeft += this.scrollerRef.current.scrollWidth - scrollWidth)
+        )
     }
     goForward(){
         this.setState({rangeT: this.state.rangeT + 7});
@@ -116,19 +146,27 @@ class Timeline extends React.Component{
 
     handleScroll() {
 
-        const s = this.scrollerRef.current.scrollLeft;
-
-        if(s < 500){
-
-            this.goBack()
-        }
-        else if(this.scrollerRef.current.scrollWidth - s - this.scrollerRef.current.offsetWidth < 1000){
+        if(this.state.isLoading === false){
             
-            this.goForward();
+            const s = this.scrollerRef.current.scrollLeft;
+
+            if(s < 500){
+                this.goBack();
+            }
+            else if(this.scrollerRef.current.scrollWidth - s - this.scrollerRef.current.offsetWidth < 1000){
+                this.goForward();
+            }
+            this.state.isLoading = false;
         }
     }
 
+    stringToDate(str) {
+        return new Date(`${str.substr(0, 4)}-${str.substr(4, 2)}-${str.substr(6, 2)}`);
+    }
 
+    dateToString(date) {
+        return date.toISOString().substr(0, 10).replace(/\-/g, "");
+    }
 
     render(){
         return(
@@ -150,10 +188,23 @@ class Timeline extends React.Component{
                         </div>
                         <div ref={this.scrollerRef} className="day-holder" onScroll={this.handleScroll}>
                             {
-                            range(this.state.rangeT).map((x) => (
-                                (this.isKeyDate(x))
-                                ? <div className="day-Timeline" id="key">{this.getWeekDay(x)} <br></br> {this.getMonth(x)} <br></br> {this.getNextDay(x).getDate()}</div>
-                                : <div className="day-Timeline">{this.getWeekDay(x)} <br></br> {this.getMonth(x)} <br></br> {this.getNextDay(x).getDate()}</div>
+                            this.state.days.map((x, i) => (
+                                (this.isKeyDate(i))
+                                ? <div className="day-Timeline" id="key">
+                                    {this.getWeekDay(i)} <br></br> 
+                                    {this.getMonth(i)} <br></br> 
+                                    {this.getNextDay(i).getDate()}
+                                    <div className="task-holder">
+                                        {
+                                            
+                                        }
+                                    </div>
+                                </div>
+                                : <div className="day-Timeline">
+                                    {this.getWeekDay(i)} <br></br> 
+                                    {this.getMonth(i)} <br></br> 
+                                    {this.getNextDay(i).getDate()}
+                                </div>
                                 ))
                             }
                         </div>
@@ -163,7 +214,7 @@ class Timeline extends React.Component{
         );
     }
     componentDidMount() {
-        this.scrollerRef.current.scrollLeft = document.getElementById("key").offsetLeft;
+        //this.scrollerRef.current.scrollLeft = document.getElementById("key").offsetLeft;
         this.fetchProjectsAndTasksToState();
     }
 }
