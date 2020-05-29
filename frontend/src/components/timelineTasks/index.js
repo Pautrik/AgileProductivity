f743275ec393a63290871644bc2c76945acc8f86
pautrik <patrikemanuelson@gmail.com>
2020-05-29 14:11:59 +0200

    Added ability to delete timeline task

--- frontend/src/components/timelineTasks/index.js
+++ frontend/src/components/timelineTasks/index.js
@@ -12,25 +12,30 @@ class TimelineTasks extends React.Component {
 
     return (
       <div className="timeline-tasks">
-        {tasks.map(task => {
-          // const stylePrep = { width: `calc(100% * ${taskLength})`, height: `calc(100% / ${tasks.length})`};
-          const stylePrep = { height: `calc(100% / ${heightSplit})`};
-          if(task !== undefined) {
-            const taskWidth = dayDistance(stringToDate(task.date), stringToDate(task.endDate));
-            stylePrep.width = `calc(100% * ${taskWidth + 1})`;
-          }
-          else {
-            stylePrep.visibility = "hidden";
-          }
-
-          return (
-            <div className="timeline-task" style={stylePrep}>
-              {task && <p>{task.text}</p>}
-            </div>
-          )
-        })}
+        {tasks.map(t => this.renderTask(heightSplit, t))}
       </div>
     );
+  }
+
+  renderTask(heightSplit, task) {
+    const stylePrep = { height: `calc(100% / ${heightSplit})` };
+
+    if(task !== undefined) {
+      const taskWidth = dayDistance(stringToDate(task.date), stringToDate(task.endDate));
+      stylePrep.width = `calc(100% * ${taskWidth + 1})`;
+    }
+    else {
+      stylePrep.visibility = "hidden";
+    }
+
+    console.log(this.props);
+
+    return (
+      <div className="timeline-task" style={stylePrep}>
+        {task && <p>{task.text}</p>}
+        <button onClick={() => this.props.deleteTask(task.id)}>&#128465;</button>
+      </div>
+    )
   }
 }
 
