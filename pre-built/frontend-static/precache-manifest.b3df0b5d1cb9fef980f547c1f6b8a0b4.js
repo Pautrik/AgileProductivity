bbc5ffbe6146bf9d2330acf731aa0bee0d34417a
pautrik <>
2020-06-05 14:09:29 +0100

    Added pre-built resources

---
+++ pre-built/frontend-static/precache-manifest.b3df0b5d1cb9fef980f547c1f6b8a0b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48fb0850a3e11cbd38023270f79240ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d0bbc71cd764dec0547",
+    "url": "/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "949ed5ed2b04d4422730",
+    "url": "/static/css/main.6cabd5b0.chunk.css"
+  },
+  {
+    "revision": "9d0bbc71cd764dec0547",
+    "url": "/static/js/2.c54e1226.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.c54e1226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "949ed5ed2b04d4422730",
+    "url": "/static/js/main.b10f0ac9.chunk.js"
+  },
+  {
+    "revision": "49d54895f917a5349f7d",
+    "url": "/static/js/runtime-main.c80e4867.js"
+  }
+]);
